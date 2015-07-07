7c93dda5dce5bfa2406870b8365ad1f4d9625471
Ashley Ginsberg <ashley@ashleyginsberg.com>
2015-07-07 18:24:32 -0400

    Format time for query; test still fails; not returning appt

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -4,7 +4,9 @@ module Api
       appointments = Appointment.all
       if start_time = params[:start_time]
         start_time = format_time(start_time)
-        appointments = appointments.where(start_time: start_time.beginning_of_day..start_time.end_of_day )
+        beg_day = start_time.beginning_of_day
+        end_day = start_time.end_of_day
+        appointments = appointments.where(start_time: string_time(beg_day)..string_time(end_day))
       end
       render json: appointments, status: 200
     end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,12 @@ class ApplicationController < ActionController::Base
 
   def format_time(time)
     time.include?('%20') ? time.gsub('%20', ' ') : time
-    Time.strptime(time, "%m/%d/%y %H:%M") #Converts string mm/dd/yy to time format
+    Time.strptime(time, "%m/%d/%y %k:%M") #Converts string mm/dd/yy to time format
   end
-  
+
+  def string_time(time)
+    time.strftime("%m/%d/%y %k:%M") #Convert time to string format mm/dd/yy
+  end  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 end
