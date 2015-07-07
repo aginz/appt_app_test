67d5a5d26af1ff27bbeee60e3980009b9d4ff1b5
Ashley Ginsberg <ashley@ashleyginsberg.com>
2015-07-07 16:39:10 -0400

    Add filter to index v1

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -2,6 +2,10 @@ module Api
   class AppointmentsController < ApplicationController
     def index
       appointments = Appointment.all
+      if start_time = params[:start_time]
+        start_time = format_time(start_time)
+        appointments = appointments.where(start_time: start_time.beginning_of_day..start_time.end_of_day )
+      end
       render json: appointments, status: 200
     end
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :format_time
+
+  def format_time(time)
+    time.include?('%20') ? time.gsub('%20', ' ') : time
+    Time.strptime(time, "%m/%d/%y %H:%M") #Converts string mm/dd/yy to time format
+  end
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
 end
