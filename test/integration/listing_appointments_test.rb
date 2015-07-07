1a976d2af035b76e07ff6181af45d39d73882ebe
Ashley Ginsberg <ashley@ashleyginsberg.com>
2015-07-07 15:52:20 -0400

    Fix filter by start_date test

--- test/integration/listing_appointments_test.rb
+++ test/integration/listing_appointments_test.rb
@@ -9,15 +9,15 @@ class ListingAppointmentsTest < ActionDispatch::IntegrationTest
     refute_empty response.body
   end
 
-  test 'returns appointments filtered by start_time'
-    test_appt1 = Appointment.create!(start_time: 11/11/15 9:00, end_time: 11/11/15 9:30, first_name: 'nov_test', last_name: 'nov_example', comments:)
-    test_appt2 = Appointment.create!(start_time: 10/11/15 10:00, end_time: 10/11/15 10:30, first_name: 'oct_test', last_name: 'oct_example', comments:)
+  test 'returns appointments filtered by start_time' do
+    test_appt1 = Appointment.create!(start_time: '11/11/15 9:00', end_time: '11/11/15 9:30', first_name: 'nov_test', last_name: 'nov_example', comments: '')
+    test_appt2 = Appointment.create!(start_time: '10/11/15 10:00', end_time: '10/11/15 10:30', first_name: 'oct_test', last_name: 'oct_example', comments: '')
 
-    get '/appointments?start_time=11/11/15'
+    get '/appointments?start_time=11/11/15 9:00'
     assert_equal 200, response.status
 
     appointments = JSON.parse(response.body, symbolize_first_names: true)
-    first_names = appointments.collect { |a| z[:name] }
+    first_names = appointments.collect { |a| a[:first_name] }
     assert_includes first_names, 'nov_test'
     refute_includes first_names, 'oct_test'
   end
