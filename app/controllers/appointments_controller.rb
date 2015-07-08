50372bf0775291c5c9827284e828eb11033936ff
Ashley Ginsberg <ashley@ashleyginsberg.com>
2015-07-08 12:37:55 -0400

    Add create method to appointment controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,7 +3,18 @@ class AppointmentsController < ApplicationController
     @appointments = Appointment.all
     respond_to do |format|
       format.json
-      format.html
     end
   end
+
+  def create
+    appointment = Appointment.new(appointment_params)
+    if appointment.save
+      render json: appointment, status: 201, location: appointment
+    end
+  end
+
+  private
+    def appointment_params
+      params.require(:appointment).permit(:start_time, :end_time, :first_name, :last_name, :comments)
+    end
 end
