be0a5a3e30d4b11dcdb5599824de02d669e8663b
Ashley Ginsberg <ashley@ashleyginsberg.com>
2015-07-14 15:50:20 -0400

    add validation tests for presensce of params

--- test/integration/creating_appointments_test.rb
+++ test/integration/creating_appointments_test.rb
@@ -15,51 +15,7 @@ class CreatingAppointmentsTest < ActionDispatch::IntegrationTest
     appointment = JSON.parse(response.body, symbolize_names: true)
     assert_equal appointment_url(appointment[:id]), response.location
   end
-  
-  test 'does not create appointments with nil start_time' do
-    post '/appointments',
-      { appointment:
-        { start_time: nil, end_time: "1/1/16 9:05", first_name: "Jack", last_name: "Example", comments: "" }
-      }.to_json,
-      { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
 
-    assert_equal 422, response.status
-    assert_equal Mime::JSON, response.content_type 
-  end
-
-  test 'does not create appointments with nil end_time' do
-    post '/appointments',
-      { appointment:
-        { start_time: "1/1/16 9:00", end_time: nil, first_name: "Jack", last_name: "Example", comments: "" }
-      }.to_json,
-      { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
-
-    assert_equal 422, response.status
-    assert_equal Mime::JSON, response.content_type 
-  end
-
-  test 'does not create appointments with nil first_name' do
-    post '/appointments',
-      { appointment:
-        { start_time: "1/1/16 9:00", end_time: "1/1/16 9:05", first_name: nil, last_name: "Example", comments: "" }
-      }.to_json,
-      { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
-
-    assert_equal 422, response.status
-    assert_equal Mime::JSON, response.content_type 
-  end
-  
-  test 'does not create appointments with nil last_name' do
-    post '/appointments',
-      { appointment:
-        { start_time: "1/1/16 9:00", end_time: "1/1/16 9:05", first_name: "Jack", last_name: nil, comments: "" }
-      }.to_json,
-      { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
-
-    assert_equal 422, response.status
-    assert_equal Mime::JSON, response.content_type 
-  end
-  
   test 'does not create appointments with start time in past' do
     post '/appointments',
       { appointment:
