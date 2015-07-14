882c484d3b06f8bfc92c0c97b762887bd6254c35
Ashley Ginsberg <ashley@ashleyginsberg.com>
2015-07-14 15:49:04 -0400

    remove subdomain from listing test setup

--- test/integration/listing_appointments_test.rb
+++ test/integration/listing_appointments_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
 class ListingAppointmentsTest < ActionDispatch::IntegrationTest
-  setup { host! 'api.appt-test.dev' }
-
   test 'returns list of all appointments' do
     get '/appointments'
     assert_equal 200, response.status
