e0859cff7cee1e0da332d9abac431f76e825e39a
Ashley Ginsberg <ashley@ashleyginsberg.com>
2015-07-14 15:49:31 -0400

    Fix typo in model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,11 +1,11 @@
 class Appointment < ActiveRecord::Base
   validates :start_time, :end_time, :first_name, :last_name, presence: true
   validate :appointment_cannot_be_in_the_past, :appointment_cannot_exist, on: :create
-  validate :appointment_cannot_be_in_the_past, :appointment_connot_exist, on: :update
+  validate :appointment_cannot_be_in_the_past, :appointment_cannot_exist, on: :update
 
   private
   def appointment_cannot_be_in_the_past
-    appointment_time = format_time(:start_time)
+    appointment_time = format_time(@appointment.start_time)
 
     if appointment_time < Time.now
       errors.add(:start_time, "can't be in the past")
@@ -13,7 +13,7 @@ class Appointment < ActiveRecord::Base
   end
 
   def appointment_cannot_exist
-    appointment_time = :start_time
+    appointment_time = @appointment.start_time
     if Appointment.find_by(start_time: appointment_time) == !nil
       errors.add(:start_time, "appointment already exists")
     end
