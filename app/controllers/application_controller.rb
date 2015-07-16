45f4912efc72e2a2535fa7522c9dcbd01c263610
Ashley Ginsberg <ashley@ashleyginsberg.com>
2015-07-16 17:24:46 -0400

    Add validation for past appointments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
-  # def format_url_time(time)
-    # time.include?('%20') ? time.gsub('%20', ' ') : time #Convert URL query to proper format
-    # convert_time(time)
-  # end
-  
-  #Converts to DateTime object
+  #Converts to DateTime object from string
   def convert_time_from_string(old_time)
     DateTime.parse(old_time) 
   end
-
-  # def string_time(time)
-    # time.strftime("%m/%e/%y %k:%M") #Convert time to string format mm/dd/yy
-  # end  
 end
